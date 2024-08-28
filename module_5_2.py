a0f670cb57c4152ca87a73c02979ef88600b37ae
Flyzja <mamontova-75@mail.ru>
2024-08-28 16:45:58 +0300

    Специальные методы классов

--- module_5_2.py
+++ module_5_2.py
@@ -11,11 +11,21 @@ class House:
 
                     return new_floor
 
+    def __len__(self):
+        return int(self.number_of_floors)
+
+    def __str__(self):
+        return f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.'
 
 cucumber = House('ЖК Огурец', 25)
 space = House('ЖК Космос', 75)
 cucumber.go_to(48)
 space.go_to(3)
 
+# __str__
+print(cucumber)
+print(space)
 
-
+# __len__
+print(len(cucumber))
+print(len(space))
