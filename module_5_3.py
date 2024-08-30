eb0545e87634e779beca3085273f41c122c7b8a1
Flyzja <mamontova-75@mail.ru>
2024-08-30 15:07:37 +0300

    доработка

--- module_5_3.py
+++ module_5_3.py
@@ -1,15 +1,17 @@
 class House:
+
     def __init__(self, name, number_floors):
         self.name = name
         self.number_of_floors = number_floors
+
     def go_to(self, new_floor):
         for f in range(new_floor + 1):
-            if f <= self.number_of_floors and f >=  1:
+            if f <= self.number_of_floors and f >= 1:
                 print(f)
             if new_floor > self.number_of_floors or self.number_of_floors < 1:
-                    print('Такого этажа не существует')
+                print('Такого этажа не существует')
 
-                    return new_floor
+                return new_floor
 
     def __len__(self):
         return int(self.number_of_floors)
@@ -18,26 +20,52 @@ class House:
         return f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.'
 
     def __eq__(self, other):
-        return (self.number_of_floors == other.number_of_floors)
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
+        # return (self.number_of_floors == other.number_of_floors)
 
     def __lt__(self, other):
-        return (self.number_of_floors < other.number_of_floors)
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        # return (self.number_of_floors < other.number_of_floors)
 
     def __le__(self, other):
-        return (self.number_of_floors <= other.number_of_floors)
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors <= other
+        # return (self.number_of_floors <= other.number_of_floors)
 
     def __gt__(self, other):
-        return (self.number_of_floors > other.number_of_floors)
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors > other
+        # return (self.number_of_floors > other.number_of_floors)
 
     def __ge__(self, other):
-        return (self.number_of_floors >= other.number_of_floors)
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
+        # return (self.number_of_floors >= other.number_of_floors)
 
     def __ne__(self, other):
-        return (self.number_of_floors != other.number_of_floors)
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors != other
+        # return (self.number_of_floors != other.number_of_floors)
 
     def __add__(self, value):
         if isinstance(value, int):
             self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
         return self
 
     def __radd__(self, value):
@@ -46,8 +74,10 @@ class House:
     def __iadd__(self, value):
         return self.__add__(value)
 
+
 cucumber = House('ЖК Огурец', 25)
 space = House('ЖК Космос', 75)
+
 cucumber.go_to(48)
 space.go_to(3)
 
@@ -55,27 +85,26 @@ space.go_to(3)
 print(cucumber)
 print(space)
 
-
 # __len__
 print(len(cucumber))
 print(len(space))
 
-#__eq__
+# __eq__
 print(cucumber == space)
 
-#__lt__
+# __lt__
 print(cucumber < space)
 
-#__le__
+# __le__
 print(cucumber <= space)
 
-#__gt__
+# __gt__
 print(cucumber > space)
 
-#__ge__
+# __ge__
 print(cucumber >= space)
 
-#__ne__
+# __ne__
 print(cucumber != space)
 
 print(cucumber.__add__(7))
---
+++ module_5_4.py
@@ -0,0 +1,117 @@
+class House:
+    houses_history = []
+
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        cls.args = 'data'
+        cls.kwargs = {'second': 25, 'third': 3.14}
+        print(*cls.houses_history)
+        print(*cls.args)
+        print(*cls.kwargs)
+        return super().__new__(cls)
+
+    def __init__(self, first, second, third):
+        print(first)
+        print(second)
+        print(third)
+
+    def __init__(self, name, number_floors):
+        self.name = name
+        self.number_of_floors = number_floors
+    def go_to(self, new_floor):
+        for f in range(new_floor + 1):
+            if f <= self.number_of_floors and f >=  1:
+                print(f)
+            if new_floor > self.number_of_floors or self.number_of_floors < 1:
+                    print('Такого этажа не существует')
+
+                    return new_floor
+
+    def __len__(self):
+        return int(self.number_of_floors)
+
+    def __str__(self):
+        return f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.'
+
+    def __eq__(self, other):
+        return (self.number_of_floors == other.number_of_floors)
+
+    def __lt__(self, other):
+        return (self.number_of_floors < other.number_of_floors)
+
+    def __le__(self, other):
+        return (self.number_of_floors <= other.number_of_floors)
+
+    def __gt__(self, other):
+        return (self.number_of_floors > other.number_of_floors)
+
+    def __ge__(self, other):
+        return (self.number_of_floors >= other.number_of_floors)
+
+    def __ne__(self, other):
+        return (self.number_of_floors != other.number_of_floors)
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def __del__(self):
+        print(self.name, 'cнесён, но он останется в истории')
+
+cucumber = House('ЖК Огурец', 25)
+space = House('ЖК Космос', 75)
+
+cucumber.go_to(48)
+space.go_to(3)
+
+# __str__
+print(cucumber)
+print(space)
+
+
+# __len__
+print(len(cucumber))
+print(len(space))
+
+#__eq__
+print(cucumber == space)
+
+#__lt__
+print(cucumber < space)
+
+#__le__
+print(cucumber <= space)
+
+#__gt__
+print(cucumber > space)
+
+#__ge__
+print(cucumber >= space)
+
+#__ne__
+print(cucumber != space)
+
+print(cucumber.__add__(7))
+print(space.__add__(-7))
+
+print(cucumber.__radd__(-7))
+print(space.__radd__(7))
+
+print(cucumber.__iadd__(3))
+print(space.__iadd__(3))
+
+birdhouse = House('ЖК Скворечник', 44)
+print(House.houses_history[0], '- первое здание')
+print(House.houses_history[1], '- второе здание')
+print(House.houses_history[-1], '- третье здание')
+
+
+
+
